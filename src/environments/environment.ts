c0ccd43ccb6cacb5d2434f0503287c46f30548c8
Brian Gammon <bgammon@gmail.com>
2017-10-21 14:41:43 -0400

    set up ci/cd for dev branch

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDnhjC9pfqqIvGObcUlN9KUIX7khkYpmBY',
+    authDomain: 'cluckrdev.firebaseapp.com',
+    projectId: 'cluckrdev',
+    databaseURL: 'https://cluckrdev.firebaseio.com',
+    storageBucket: 'cluckrdev.appspot.com'
+  }
+};
+
